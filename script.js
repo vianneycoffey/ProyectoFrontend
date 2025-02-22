66cd65692e87b4f6346307728d53428bababe914
vianneycoffey <vianney.saldanacoffey@cesunbc.edu.mx>
2025-02-21 19:13:24 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function Si(){
+    document.getElementById("texto").innerText = "Gracias por tu opinion ♥"
+}
+function No(){
+    document.getElementById("texto").innerText = "Gracias por tu opinion ☹"
+}
+
